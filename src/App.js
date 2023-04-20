36eb508730d29e4019a501b6448f127e1e53e633
Med-Amin16 <moumou-1998@hotmail.fr>
2023-04-20 06:09:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ToDoList from "./Components/ToDoList";
+import AddToDo from "./Components/AddToDo";
+import "./App.css";
+import { filterTodo } from "./Redux/Actions/todoActions";
+import cx from "classnames";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const todos = useSelector((state) => state.toDoReducer.todos);
+    const filter = useSelector((state) => state.filterReducer);
+    const dispatch = useDispatch();
+    // console.log("todos1:",todos);
+    return (
+        <div className="App">
+            <h1>TODO LIST</h1>
+            <AddToDo />
+            <div className="Filters">
+                <button
+                    className={cx("Filters-Btn", {
+                        selected: filter === "ALL",
+                    })}
+                    onClick={() => dispatch(filterTodo("ALL"))}
+                >
+                    ALL
+                </button>
+                <button
+                    className={cx("Filters-Btn", {
+                        selected: filter === true,
+                    })}
+                    onClick={() => dispatch(filterTodo(true))}
+                >
+                    Done
+                </button>
+                <button
+                    className={cx("Filters-Btn", {
+                        selected: filter === false,
+                    })}
+                    onClick={() => dispatch(filterTodo(false))}
+                >
+                    NotDone
+                </button>
+            </div>
+            <div className="ToDoList">
+                <ToDoList
+                    todos={
+                        filter === "ALL"
+                            ? todos
+                            : todos.filter((el) => el.isDone === filter)
+                    }
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
